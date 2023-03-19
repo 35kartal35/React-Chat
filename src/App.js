10a33b6fb8f848f6ae5917b40c8413746798f98b
35kartal35 <mozdemir3545@gmail.com>
2023-03-19 18:11:01 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 
-import Auth from './component/Auth';
+import { useRef, useState } from 'react';
+
+import Auth from './components/Auth';
+import Chat from './components/chat';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('token'));
+  const [room, setRoom] = useState(null);
 
+  const inputRef = useRef(null);
+  // eğer kullanıcı henüz giriş yapmadıysa yönlendir
+  if (!isAuth) {
+    return (
+      <div className="container">
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
 
+  // eğer giriş yaptıysa
   return (
-    <div className="App">
-      <Auth />
+    <div className="container">
+      {room ? (
+        <Chat room={room} />
+      ) : (
+        <div className="room-container">
+          <h1>Chat Odası</h1>
+          <p>Hangi Odaya Gireceksin ?</p>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="oda ismini yazınız..."
+          />
+          <button onClick={() => setRoom(inputRef.current.value)} id="enter">
+            Odaya Gir
+          </button>
+          <button id="leave">Çıkış Yap</button>
+        </div>
+      )}
     </div>
   );
 }
